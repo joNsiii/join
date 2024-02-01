0152dbb98cb7d6d1ba9b2a733b78a39f2f319360
printjanik <janikstratmann99@gmail.com>
2024-02-01 21:55:29 +0100

    startup / log-in page update

--- logo-animation.js
+++ logo-animation.js
@@ -1,13 +1,18 @@
 function renderLogin() {
     const logo = document.querySelector('.joinLogo');
+    const startup = document.querySelector('.startup');
 
     setTimeout(function(){
         logo.classList.add('selected');;
     }, 1000);
 
     setTimeout(function(){
-        logo.classList.add('selected');;
+        startup.classList.add('blend-out');;
     }, 1250);
+
+    setTimeout(function(){
+        startup.classList.add('d-none');;
+    }, 1350);
 }
 
 
