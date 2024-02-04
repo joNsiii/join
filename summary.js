b3f6965c08a5531a5a88d9b5d09db26516b91572
joNsiii <jens.gartsch@gmail.com>
2024-02-04 01:15:56 +0100

    adding hover effects to summary

---
+++ summary.js
@@ -0,0 +1,18 @@
+function changeImage() {
+    let editImg = document.getElementById('edit-img');
+    editImg.src = 'img/edit-bg-white.png'   
+}
+function changeImageBack() {
+    let editImg = document.getElementById('edit-img');
+    editImg.src = 'img/edit-bg-blue.png'
+}
+
+function changeImage1() {
+    let successImage = document.getElementById('success-img');
+    successImage.src = 'img/success-bg-white.png';
+}
+function changeImageBack1() {
+    let successImage = document.getElementById('success-img');
+    successImage.src = 'img/success-bg-blue.png'
+}
+
