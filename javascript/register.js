da66b903ace2892e19d734b8ca509f66f1c15eeb
joNsiii <jens.gartsch@gmail.com>
2024-02-08 19:14:23 +0100

    signup implemented

--- javascript/register.js
+++ javascript/register.js
@@ -1,2 +1,26 @@
 let users = [];
 
+async function addUser() {
+
+    let name = document.getElementById('username');
+    let email = document.getElementById('useremail');
+    let password = document.getElementById('password-type');
+    if(users.find(u => name.value === u.name)) {
+        return alert('Username already exist!!')    
+    }if(users.find(u => email.value === u.email)) {
+        return alert('Email already exist!!')    
+    }
+    users.push({name: name.value, email: email.value, password: password.value})
+    alert('Your account was successfully created!!!')
+    // window.location.reload();
+}
+
+function checkPassword() {
+    let password = document.getElementById('password-type').value;
+    let confirmPassword = document.getElementById('password-type2').value;
+    if(password === confirmPassword) {
+        addUser();
+    }else {
+        alert('PASSWORDS ARE DIFFERENT!!!')
+    }
+}
