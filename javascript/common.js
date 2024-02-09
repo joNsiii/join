b5346a05974ae5af4f3821e14ed42049d4216426
Your Name <you@example.com>
2024-02-09 16:44:07 +0100

    added addClass() and setElementAttribute()

--- javascript/common.js
+++ javascript/common.js
@@ -1,21 +1,20 @@
 /**
+ * Adds a class to the element 'id'.
+ * @param {value} id - The element's id.
+ * @param {String} className - The adding class name.
+ */
+function addClass(id, className) {
+    document.getElementById(id).classList.add(className);
+}
+
+
+/**
  * Provides an element by id.
  * @param {String} id - The requested element's id.
  * @returns - The requested element.
  */
 function getElement(id) {
     return document.getElementById(id);
-}
-
-
-/**
-//  * Provides a json's object value.
- * @param {json} variable - The providing json.
- * @param {value} key - The requested object's key.
- * @returns - The requested object's value.
- */
-function getJsonObjectValue(variable, key) {
-    return variable[key];
 }
 
 
@@ -31,6 +30,23 @@ function getJsonObjectDeepValue(variable, key, subkey) {
 }
 
 
-function addClass(id, className) {
-    document.getElementById(id).classList.add(className);
+/**
+//  * Provides a json's object value.
+ * @param {json} variable - The providing json.
+ * @param {value} key - The requested object's key.
+ * @returns - The requested object's value.
+ */
+function getJsonObjectValue(variable, key) {
+    return variable[key];
+}
+
+
+/**
+ * Sets an element's attribute.
+ * @param {value} id - The element's id.
+ * @param {String} attribute - The setting attribute.
+ * @param {value} value - The setting value.
+ */
+function setElementAttribute(id, attribute, value) {
+    document.getElementById(id).setAttribute(attribute, value);
 }
