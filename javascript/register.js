fa56253acfebf57f26363d1ad194f775d972422e
joNsiii <jens.gartsch@gmail.com>
2024-02-10 00:27:38 +0100

    login and signup in progress

--- javascript/register.js
+++ javascript/register.js
@@ -1,25 +1,38 @@
 let users = [];
 
-async function addUser() {
 
+
+async function addUser() {
+    let date = new Date();
+    let userId = date.getTime();
+    let newArray = []; /* delete our backend-users-array */
     let name = document.getElementById('username');
     let email = document.getElementById('useremail');
     let password = document.getElementById('password-type');
-    if(users.find(u => name.value === u.name)) {
-        return alert('Username already exist!!')    
-    }if(users.find(u => email.value === u.email)) {
-        return alert('Email already exist!!')    
+    if (users.find(u => name.value === u.name)) {
+        return alert('Username already exist!!');
+    } if (users.find(u => email.value === u.email)) {
+        return alert('Email already exist!!');
     }
-    users.push({name: name.value, email: email.value, password: password.value})
-    window.location.href = 'login.html?msg=You Signed Up successfully'
+   
+    let newUser = {
+        'name': name.value,
+        'email': email.value,
+        'password': password.value,
+        'id': userId
+    }
+    users.push(newUser);
+    // window.location.href = 'login.html?msg=You Signed Up successfully'
+
+    setItem('users', users);
 }
 
 function checkPassword() {
     let password = document.getElementById('password-type').value;
     let confirmPassword = document.getElementById('password-type2').value;
-    if(password === confirmPassword) {
+    if (password === confirmPassword) {
         addUser();
-    }else {
+    } else {
         alert('PASSWORDS ARE DIFFERENT!!!')
     }
 }
