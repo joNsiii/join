99e78e001df86aa5bb66a98ef148be1cab6e5e85
CodeArcher204 <codearcher204@gmail.com>
2024-02-24 19:15:28 +0100

    dbt changed to bgc in board.js

--- javascript/board.js
+++ javascript/board.js
@@ -263,7 +263,7 @@ function generateSubUsersHtml(subUsers) {
             const fullName = user.name;
             subUserNamesHtml += /*html*/ `
                 <div class="dbt-contact-group">
-                        <div class="dbt-contact-profile dbt-${user.userBackgroundColor}">${initials}</div>
+                        <div class="dbt-contact-profile bgc-${user.userBackgroundColor}">${initials}</div>
                         <div class="dbt-contact-name">${fullName}</div>
                 </div>
             `;
@@ -303,7 +303,7 @@ function generateUserInitialsHtml(subUsers) {
                 .match(/(\b\S)?/g)
                 .join("")
                 .toUpperCase();
-            userInitialsHtml += `<div class="board-card-user bcu-${user.userBackgroundColor}">${initials}</div>`;
+            userInitialsHtml += `<div class="board-card-user bgc-${user.userBackgroundColor}">${initials}</div>`;
         }
         return userInitialsHtml;
     }
