94d51792cf88fe591c4af360680cca14680f2f03
Your Name <you@example.com>
2024-02-28 16:56:16 +0100

    started with selectAssignableContacts()

--- javascript/add-task-form.js
+++ javascript/add-task-form.js
@@ -40,7 +40,7 @@ function pushAssignableUserContacts(user) {
 
 
 function renderAssignableContacts() {
-    let assigningSelection = document.getElementById('assignable-contacts-form');
+    let assigningSelection = document.getElementById('assignable-contacts-collector');
     assigningSelection.innerHTML = '';
     fillAssigningSelection(assigningSelection);
 }
@@ -49,8 +49,35 @@ function renderAssignableContacts() {
 function fillAssigningSelection(assigningSelection) {
     for (let i = 0; i < assignableContacts.length; i++) {
         let contact = assignableContacts[i];
+        let initialLetters = getInitialLetterGroup(contact);
         assigningSelection.innerHTML += `
-            <option value="${i}">${contact.name}</option>
+            <div id="assignable-contact-${i}" class="assignable-contact-group" onclick="selectAssignableContact(${i}, true)">
+                <div class="assignable-contact">
+                    <div class="assignable-contact-initials bgc-${contact['bgc-name']}">${initialLetters}</div>
+                    <div class="assignable-contact-name">${contact.name}</div>
+                </div>
+                <img id="assignable-contact-checkbox-${i}" src="../img/check-button-false.png" alt="check-button-false">
+            </div>
         `;
+    }
+}
+
+
+function getInitialLetterGroup(contact) {
+    let nameGroup = contact.name.split(' ');
+    let name = nameGroup[0][0] + nameGroup[nameGroup.length - 1][0];
+    return name;
+}
+
+
+function selectAssignableContact(i, selecting) {
+    let image = document.getElementById(`assignable-contact-checkbox-${i}`);
+    let contact = document.getElementById(`assignable-contact-${i}`);
+    if (selecting) {
+        image.src = '../img/check-button-true.png';
+        contact.setAttribute('onclick', `selectAssignableContact(${i}, false)`);
+    } else {
+        image.src = '../img/check-button-false.png';
+        contact.setAttribute('onclick', `selectAssignableContact(${i}, true)`);
     }
 }
